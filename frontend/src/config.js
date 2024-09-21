b37915c88b16ab4eaeb2d215efa94dc57984f08b
melencholicmice <learningwithturtlespeedcode@gmail.com>
2024-09-21 10:43:03 +0530

    fixes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://muta-events.onrender.com/";
+export const API_URL = "https://muta-events.onrender.com";
 export const RECAPTCHA_SITE_KEY = "6LcckkoqAAAAAA0DKIemexRY2RChaFggb1NemRR0"
